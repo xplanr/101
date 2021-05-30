90a80cb4e2074102922fd73ea9734ec21e01dbea
Tim Menzies <timm@ieee.org>
2021-05-30 12:30:30 -0400

    save

---
+++ test/rbesteg.py
@@ -0,0 +1,20 @@
+# vim: filetype=python ts=2 sw=2 sts=2 et :
+from rbest import rbest
+from tab  import Tab
+import about
+
+def eg1():
+  the=about.defaults()
+  t=Tab(file=the.data)
+  print(the.data)
+  for _ in  range(10):
+    print("")
+    rbest(t,the)
+
+#go()
+#go("data/auto93.csv")
+#diveg("data/auto93.csv")
+#diveg("data/pom_dataset.csv")
+#diveg("data/xomo_dataset.csv")
+
+eg1()
